f1efeb6ed2e9dae336cf588471a4dac72e661845
bwesterg <benton.westergaard@gmail.com>
2023-12-12 22:12:13 -0700

    testing node and nodemon problem

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -2,6 +2,7 @@ export default function LoginPage() {
     return (
         <form className="login">
             <h1>Login</h1>
+            <p>test text</p>
             <input type="text" placeholder="username"/>
             <input type="password" placeholder="password"/>
             <button>Login</button>

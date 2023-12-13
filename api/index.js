186445db83ec604033a32c8b1943c029fb7901a1
bwesterg <benton.westergaard@gmail.com>
2023-12-13 15:27:39 -0700

    adds another stopping point. verifies that login checks password and login against DB correctly and returns true or false network response.

--- api/index.js
+++ api/index.js
@@ -15,18 +15,23 @@ mongoose.connect('mongodb+srv://blog:Mhmr3NNYIOmqSJAU@cluster0.tb81blt.mongodb.n
 app.post('/register', async (req, res) => {
     const {username, password} = req.body;
     try {
-        const userDoc =  await User.create({
+        const userDoc = await User.create({
             username,
             password:bcrypt.hashSync(password,salt),
-        })
-        res.json (userDoc);
+        });
+        res.json(userDoc);
     } catch(e){
+        console.log(e);
         res.status(400).json(e);
     }
 });
 
-app.get('/test', (req, res) => {
-    res.json('test ok dawg');
+
+app.post('/login', async (req,res) => {
+    const {username,password} = req.body;
+    const userDoc = await User.findOne({username});
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    res.json(passOk);
 });
 
 app.listen(4000);
